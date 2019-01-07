20e4ede7c25db8745ad86b1b5c01706538dc04bd
Rafael Medrano <omnitrixupgrade@gmail.com>
2019-01-07 23:13:00 +0000

    Functions

--- js/script.js
+++ js/script.js
@@ -9,6 +9,27 @@ $(document).ready(function() {
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
         var totalScore;
+        
+        if (q1Result.length <= 4){
+           totalScore = totalScore + 1;
+        }
+        else if (q1Result.length > 4 && q1Result.length <= 10){
+           totalScore = totalScore + 2
+        }
+        else if(q1Result.length > 10){
+           totalScore = totalScore + 3
+        }
+        
+        if (q1Result.length <= 4){
+           totalScore = totalScore + 1;
+        }
+        else if (q1Result.length > 4 && q1Result.length <= 10){
+           totalScore = totalScore + 2
+        }
+        else if(q1Result.length > 10){
+           totalScore = totalScore + 3
+        }
+        
     });
 
 });
